727fe202b49317df3c3c97797b943895d9b04697
seopport <syounseok94@gmail.com>
2023-09-15 12:02:09 +0900

    5장 추가

---
+++ DO it! 점프 투 자바/05장 객체 지향 프로그래밍/CounterO.java
@@ -0,0 +1,21 @@
+class Updater {
+    void update(Counter counter) {
+        counter.count ++;
+    }
+}
+
+class Counter {
+    int count = 0;
+}
+
+public class Sample {
+    public static void main(String[] args) {
+
+        Counter myCounter = new Counter();
+        System.out.println("before update : " + myCounter.count);
+
+        Updater myUpdater = new Update();
+        myUpdater.update(myCounter);
+        System.out.println("after update : " + myCounter.count);
+    }
+}
